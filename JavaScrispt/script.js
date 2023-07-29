b6001f3c876cfd34172515990b84aa7ee64b2c9f
TobiasTLRT <133592076+TobiasTLRT@users.noreply.github.com>
2023-07-29 14:50:44 -0300

    Add files via upload

---
+++ JavaScrispt/script.js
@@ -0,0 +1,19 @@
+function inclusive() {
+    document.body.style.backgroundColor = "black";
+    document.getElementById("main").style.backgroundColor = "black";
+    document.getElementById("header").style.backgroundColor = "black";
+    document.getElementById("header").style.color = "#90ffe9";
+    document.getElementById("section").style.backgroundColor = "black";
+    document.getElementById("section").style.color = "#90ffe9";
+    document.getElementById("section1").style.backgroundColor = "black";
+    document.getElementById("section1").style.color = "#90ffe9";
+    document.getElementById("section2").style.backgroundColor = "black";
+    document.getElementById("section2").style.color = "#90ffe9";
+    document.getElementById("aside").style.backgroundColor = "black";
+    document.getElementById("aside").style.color = "#90ffe9";
+    document.getElementById("footer").style.backgroundColor = "black";
+    document.getElementById("footer").style.color = "#90ffe9";
+  }
+  function autor() {
+    alert("Autor: Tobias Rios, Edad:16, Email: toby.rios.3048@gmail.com ");
+    }
